fa6c1f6103d0df2ed0ec70f2517dc2d91c3f354f
imaspiderman <imaspiderman@hotmail.com>
2013-04-27 21:24:27 -0400

    started canvas class

---
+++ J3DGL/graphics/j3d_canvas.java
@@ -0,0 +1,12 @@
+package graphics;
+
+import java.awt.Window;
+
+public class j3d_canvas extends Window {
+	private static final long serialVersionUID = -7671323300003809317L;
+
+	public j3d_canvas(int width, int height){
+		super(null);
+		this.setSize(width, height);
+	}
+}
